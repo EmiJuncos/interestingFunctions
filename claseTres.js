d873e21d30d02a5eb1a892500d5c72b25ca8b84d
EmiJuncos <emilio.juncos1988@gmail.com>
2021-03-25 19:15:23 -0300

    Eliminando archivos innecesarios

--- claseTres.js
+++
@@ -1,9 +0,0 @@
-function rotation(array, rotation) {
-    for (i = 0; i < rotation; i++) {
-        var eliminado = array.shift();
-        array.push(eliminado);
-    }
-    return array;
-}
-
-console.log(rotation([1, 2, 3, 4, 5], 4));
